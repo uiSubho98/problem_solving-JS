ec30dacf5dd06d556b5c757d0668fdc326b1aafe
unknown <dsubhojit063@gmail.com>
2022-10-27 10:15:03 +0530

    reverse_string

---
+++ reverse_string.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+
+const result = reverseString("hello");
+console.log(result);
